ff9cfb7ab8e9fa112c1a509ee532957469cbb473
sklehto <susanna.lehto2@myy.haaga-helia.fi>
2022-09-21 08:16:38 +0300

    Tehtävä kesken: API key ei toimi.

--- App.js
+++ App.js
@@ -1,10 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
+
+  const [value, setValue] = useState('');
+  const [convertedValue, setConvertedValue] = useState('');
+  const [currencyCodes, setCurrencyCodes] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState('');
+
+  const myHeaders = new Headers();
+  myHeaders.append('apikey', 'Q1gkXhQTl56MKyimhFrV8Q9G6OCde4jH');
+
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+  const getConversion = () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=${selectedCurrency}&amount=${value}`, requestOptions)
+    .then(response => response.json())
+    .then(data => setConvertedValue( data.result.toFixed(2) ))
+    .catch(error => {
+      console.error(error);
+    });
+  };
+
+  const getCurrency = () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/symbols`, requestOptions)
+    .then( response => response.json() )
+    .then( data => setCurrencyCodes(data.symbols) )
+    .catch( error => {
+      console.error(error);
+    } );
+  };
+
+  useEffect(() => getCurrency(), []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Image style={styles.image} source={ require('./pixabay-euro.png') } />
+
+      <Text style={{padding: 15, fontSize: 18, fontWeight: 'bold'}}>{convertedValue} €</Text>
+
+      <View style={styles.converting}>
+      <TextInput style={styles.input} keyboardType='numeric' placeholder='Write value' onChangeText={ text => setValue(text) }
+        value={value}
+      />
+      
+        <Picker 
+          style={{ width: 100}}
+          selectedValue={selectedCurrency} 
+          onValueChange={ (itemValue, itemIndex) => setSelectedCurrency(itemValue) }
+        >
+          { Object.keys(currencyCodes).map( val => (<Picker.Item label={val} value={val} key={val} />)) }
+        </Picker>
+      </View>
+
+      <View style={styles.button}>
+        <Button title='Convert' onPress={getConversion} />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +75,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  converting: {
+    flexDirection: 'row',
+    height: 70,
+    alignItems: 'center',
+
+  },
+  image: {
+    width: 180,
+    height: 100,
+    alignSelf: 'center',
+  },
+  input: {
+    width: 120,
+    borderColor: 'grey',
+    borderWidth: 1,
+    textAlign: 'center',
+  },
+  button: {
+    width: 90,
   },
 });
